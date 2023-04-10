2aad261ba4ed07a3dade861e39d1a6fce37eaccb
Yakov Didenko <kogotto@gmail.com>
2023-04-11 01:09:24 +0400

    Add simple vertex and fragment shaders

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string_view>
 
 #define GLEW_NO_GLU
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+namespace
+{
 
 struct GLFWTerminator {
     ~GLFWTerminator() {
@@ -13,6 +17,61 @@ struct GLFWTerminator {
 constexpr int GLFW_INIT_FAILED = -1;
 constexpr int GLFW_WINDOW_CREATION_FAILED = -1;
 constexpr int GLEW_INIT_FAILED = -3;
+
+const char* shaderTypeToStr(unsigned int type) {
+    switch (type) {
+    case GL_VERTEX_SHADER:
+        return "vertex";
+    case GL_FRAGMENT_SHADER:
+        return "fragment";
+    default:
+        return "unknown shader type";
+    };
+}
+
+unsigned int compileShader(unsigned int type, std::string_view source) {
+    const unsigned int id = glCreateShader(type);
+    const char* sourceBegin = source.data();
+    const int sourceSize = source.size();
+
+    glShaderSource(id, 1, &sourceBegin, &sourceSize);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length);
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " <<
+            shaderTypeToStr(type) << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+unsigned int compileProgram(std::string_view vertexShaderSource, std::string_view fragmentShaderSource) {
+    const unsigned int id = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+    glAttachShader(id, vs);
+    glAttachShader(id, fs);
+    glLinkProgram(id);
+
+    glValidateProgram(id);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return id;
+}
+
+} // namespace
 
 int main() {
     std::cout << "Hi" << std::endl;
@@ -49,6 +108,27 @@ int main() {
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    const std::string_view vertexShaderSource =
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n"
+    ;
+
+    const std::string_view fragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 1.0, 0.0, 1.0);\n"
+        "}\n"
+    ;
+
+    unsigned int shader = compileProgram(vertexShaderSource, fragmentShaderSource);
+    glUseProgram(shader);
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
