53ac999c1f336bb0a310189c52e968bb0c681be5
Yakov Didenko <kogotto@gmail.com>
2023-04-15 16:41:06 +0400

    Introduce GLCALL macro to deal with errors

--- main.cpp
+++ main.cpp
@@ -9,6 +9,52 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+namespace {
+
+void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+struct GLErrorCode {
+    GLenum code;
+};
+
+std::ostream& operator<<(std::ostream& stream, GLErrorCode code) {
+    return stream <<
+        std::hex << std::showbase <<
+        code.code <<
+        std::noshowbase << std::dec;
+}
+
+void GLLogError(GLErrorCode code, const char* function, const char* file, int line) {
+    std::cout <<
+        "[OpenGl] error, code = " << code <<
+        " in call " << function <<
+        " on " << file << ":" << line <<
+        std::endl;
+}
+
+bool GLCheckErrors(const char* function, const char* file, int line) {
+    int errorsCount = 0;
+    for (auto errorCode = glGetError(); errorCode != GL_NO_ERROR; errorCode = glGetError()) {
+        ++errorsCount;
+        GLLogError({errorCode}, function, file, line);
+    }
+
+    return errorsCount == 0;
+}
+
+} // namespace
+
+#define GLASSERT(x) { if (!(x)) std::abort(); }
+
+#define GLCALL(x)                                    \
+{                                                    \
+    GLClearError();                                  \
+    x;                                               \
+    GLASSERT(GLCheckErrors(#x, __FILE__, __LINE__))  \
+}
+
 namespace
 {
 
