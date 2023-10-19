44b3ec7793270b5d695132250186ea8776428279
kogotto <kogotto@gmail.com>
2023-10-19 15:53:33 +0400

    Template head must be the same in out-of-class definition (#12)

--- utils/include/utils/field.hpp
+++ utils/include/utils/field.hpp
@@ -4,44 +4,44 @@
 
 namespace ut {
 
-template <typename T>
+template <std::default_initializable T>
 inline Field<T>::Field(size_type rowsCount, size_type colsCount)
     : Field(NormalizedIndex{rowsCount, colsCount})
 {}
 
-template <typename T>
+template <std::default_initializable T>
 inline Field<T>::Field(NormalizedIndex size)
     : size{size}
     , field(size.row * size.col)
 {}
 
-template <typename T>
+template <std::default_initializable T>
 template <typename U>
 Field<U> Field<T>::createWithSameSize() const {
     return {getSize()};
 }
 
-template <typename T>
+template <std::default_initializable T>
 inline auto Field<T>::cell(NormalizedIndex index) const -> const_reference {
     return field[detail::toContainerIndex(getColsCount(), index)];
 }
 
-template <typename T>
+template <std::default_initializable T>
 inline auto Field<T>::cell(NormalizedIndex index) -> reference {
     return field[detail::toContainerIndex(getColsCount(), index)];
 }
 
-template <typename T>
+template <std::default_initializable T>
 inline auto Field<T>::cell(CellIndex index) const -> const_reference {
     return cell(normalize(getSize(), index));
 }
 
-template <typename T>
+template <std::default_initializable T>
 inline auto Field<T>::cell(CellIndex index) -> reference {
     return cell(normalize(getSize(), index));
 }
 
-template <typename T>
+template <std::default_initializable T>
 inline IndexRange Field<T>::indexRange() const {
     return IndexRange{getSize()};
 }
